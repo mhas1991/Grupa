4286dda94dd5463cab922f7f3f27ab6b093b7d7f
Pawel Futa <pawelfuta@gmail.com>
2015-02-08 20:34:02 +0100

    modified:   app/config/routing.yml 	modified:   src/Grupa/ProjektBundle/Controller/IndexController.php 	modified:   src/Grupa/ProjektBundle/Resources/config/routing.yml 	src/Grupa/ProjektBundle/Resources/views/layout.html.twig

--- src/Grupa/ProjektBundle/Controller/IndexController.php
+++ src/Grupa/ProjektBundle/Controller/IndexController.php
@@ -1,3 +1,14 @@
 <?php
 
-namespace Grupa\ProjektBundle\Controller
+namespace Grupa\ProjektBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('GrupaProjektBundle:Index:index.html.twig', array());
+    }
+
+}
