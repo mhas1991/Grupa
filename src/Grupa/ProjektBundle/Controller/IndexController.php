e1e3f1e7412852900c8c2c7c02e72df82b390ae8
Pawel Futa <pawelfuta@gmail.com>
2015-02-14 10:22:04 +0100

    dzialajace wyswietlanie

--- src/Grupa/ProjektBundle/Controller/IndexController.php
+++ src/Grupa/ProjektBundle/Controller/IndexController.php
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace Grupa\ProjektBundle\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class IndexController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('GrupaProjektBundle:Index:index.html.twig', array());
-    }
-
-=======
 <?php
 
 namespace Grupa\ProjektBundle\Controller;
@@ -41,5 +26,5 @@ class IndexController extends Controller
 	{
 		return $this->render('GrupaProjektBundle:Index:recenzja.zalogowany.html.twig');
 	}
->>>>>>> d866de9b5c28131674f9ca706da61ed8b0b6ca31
+
 }
